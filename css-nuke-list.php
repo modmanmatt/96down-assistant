b44ff21981737abd3a215c83b75222baedd0200f
calicotek <calicotek@gmail.com>
2014-10-27 15:49:09 +0000

    add wpmu-dev display:none; to hide annoying update messages

--- css-nuke-list.php
+++ css-nuke-list.php
@@ -1,4 +1,15 @@
 /** CSS Nuke List Start **/
 
+<!-- WPMU Dashboard Installer -->
+#wpmu-install-dashboard {
+    display: block;
+}
+#the-list .wpmu-update-row {
+    background-color: #031F34;
+    border: medium none;
+    color: #C0D7EB;
+    background-size: 100px auto;
+    display: none;
+}
 
 /** CSS Nuke List End **/
